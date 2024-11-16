0f77f44e9715ee0d9e3253341a7b5e22750eedb3
Chukwudi M Ikem <chukwudimikem@gmail.com>
2024-11-16 01:57:57 -0800

    modified ryan's branch to include robot model with wheels, changed color, tested movement with gz topic commands works, wasd support not functional

--- act/launch/gazebo_launch.py
+++ act/launch/gazebo_launch.py
@@ -1,9 +1,9 @@
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
-    
     world_file_path = os.path.join(
         os.getenv('HOME'), 'ros2_ws', 'src', 'act', 'worlds', 'living_room.sdf'
     )
